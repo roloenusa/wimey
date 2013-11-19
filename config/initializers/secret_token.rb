13ee8e5eaa2fd857ca934f179e59cae68ad57903
Juan Delgado <juan@grizzlypanda.com>
2013-11-18 23:08:52 -0800

    Initializing Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wimey::Application.config.secret_key_base = '38f170b61993cf658d4ab51bca5d58115f26fcaca2cf73e445913410719b9b8f500a60cf4d12aa706edeae66f8205485690f5c389f76659c7d11acd4b9728937'
