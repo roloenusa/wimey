9cfd417f4dffed3e84013d6e072de636f8f9c69c
Juan Delgado <juan@grizzlypanda.com>
2013-11-22 01:02:48 -0800

    Add user formating and profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def index
   end
+  
+  def show
+    user_id = params[:id].to_i
+    if current_user && current_user.id == user_id
+      @user = current_user 
+    else 
+      @user = User.find_by_id(user_id)
+    end
+  end
+  
+  def profile
+    user_id = params[:id].to_i
+    if current_user && current_user.id == user_id
+      @user = current_user 
+    else 
+      @user = User.find_by_id(user_id)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Wimey::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root "users#index"
+  
+  resources :users do 
+    member do
+      get 'profile'
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
