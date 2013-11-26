acba28c1df03c07f40d0cd86eb759580092cd5e2
Juan Delgado <juan@grizzlypanda.com>
2013-11-25 23:20:29 -0800

    Small refactoring of user to make more sense

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,27 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :profile]
+  
   def index
   end
   
   def show
-    user_id = params[:id].to_i
-    if current_user && current_user.id == user_id
-      @user = current_user 
-    else 
-      @user = User.find_by_id(user_id)
-    end
+    @calendar_url = calendar_user_tasks_path(user_id: @user.id)
   end
   
   def profile
-    user_id = params[:id].to_i
-    if current_user && current_user.id == user_id
-      @user = current_user 
-    else 
-      @user = User.find_by_id(user_id)
-    end
+    
   end
+  
+  private
+  
+    def set_user
+      user_id = params[:id].to_i
+      if current_user && current_user.id == user_id
+        @user = current_user 
+      else 
+        @user = User.find_by_id(user_id)
+      end
+      return @user 
+    end
 end
