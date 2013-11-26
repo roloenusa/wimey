3476d35c1e831e8d8119777f7e6f20c6fa74aa42
Juan Delgado <juan@grizzlypanda.com>
2013-11-26 00:03:16 -0800

    Redirect the user to their dashboard on sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   include ApplicationHelper
+  
+  def after_sign_in_path_for(resource)
+    user_path(resource) || request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
 end
