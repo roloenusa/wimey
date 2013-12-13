ef6b4efdfc56d3a756a5d30d1bba405fa478a10b
Juan Delgado <juan@grizzlypanda.com>
2013-12-12 23:03:29 -0800

    Making things just generally prettier

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Wimey::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root "users#index"
+  root "users#home"
   
   resources :projects do
     resources :tasks do
